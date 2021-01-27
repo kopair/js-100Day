ae91e7be426a4d175fd8ee78851f13a37c913a63
kopair <32586709+kopair@users.noreply.github.com>
2021-01-27 23:26:48 +0800

    fix：完善面对对象例子
    删除tab、双击修改tab和内容

--- class/project/tab.js
+++ class/project/tab.js
@@ -11,13 +11,17 @@ class Tab {
         this.init()
     }
     init() {
+        // 更新全部的dom
         this.update()
         this.add.onclick = this.addTab;
+        // 绑定各类事件
         this.lis.forEach((item, index) => {
             item.index = index
             item.onclick = this.toggleTab;
             this.close[index].onclick = this.removeTab;
-            console.log(item);
+            // console.log(item);
+            this.spans[index].ondblclick = this.editTab;
+            this.sections[index].ondblclick = this.editTab;
         })
         
     }
@@ -25,15 +29,16 @@ class Tab {
         this.lis = this.main.querySelectorAll('li')
         this.sections = this.main.querySelectorAll('section')
         this.close = this.main.querySelectorAll('.close')
-
+        this.spans = this.main.querySelectorAll('.ulFirst li span:first-child')
     }
     // 1.切换功能
     toggleTab() {
-        console.log(this.index);
+        // console.log(this.index);
         that.clearClass()
         this.className = 'activeTab'
         that.sections[this.index].className = 'activeTabCard'
     }
+    // 清除样式
     clearClass() {
         this.lis.forEach((item, index) => {
             item.className = ''
@@ -42,7 +47,7 @@ class Tab {
     }
     // 添加tab
     addTab() {
-        console.log(1);
+        // console.log(1);
         // 创建li元素和setion元素
         that.clearClass()
         let appendTab = `<li class="activeTab"><span>初始化内容</span><span  class="close">x</span></li>`
@@ -56,15 +61,35 @@ class Tab {
         e.stopPropagation()
         let index = this.parentNode.index
         console.log(index);
+        // 删除内容和tab
         that.lis[index].remove()
         that.sections[index].remove()
+        // 让tab重新绑定事件
         that.init()
+        // 当删除非激活状态的tab
+        if(document.querySelector('.activeTab')) return 
+        // 当删除激活状态的tab
         index--
-        that.lis[index].click()
+        that.lis[index] && that.lis[index].click()
     }
     // 修改功能
     editTab() {
+        console.log('执行');
+        // 双击禁止选定文字
+        window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
 
+        let str = this.innerHTML;
+        // console.log(1);
+        this.innerHTML = `<input type="text" value=${str} />`
+        this.children[0].select()
+        this.children[0].onblur = function(){
+            this.parentNode.innerHTML = this.value
+        }
+        this.children[0].onkeyup = function(ret){
+            if(ret.keyCode === 13){
+                this.blur()
+            }
+        }
     }
 }
 let tab = new Tab('#tab')
